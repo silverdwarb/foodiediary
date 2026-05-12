ead995c0344d8e4b45e1ad53d6813c19a7b4826a
silverdwarb <stevenyang246@gmail.com>
2026-05-12 17:19:51 -0500

    forgot to add this line

--- migrations/001_add_unique_ingredient_name.sql
+++ migrations/001_add_unique_ingredient_name.sql
@@ -1,2 +1,3 @@
 ---fix missing unique constraint for ingredient table
 ALTER TABLE ingredients ADD CONSTRAINT unique_ingredient_name UNIQUE (name);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_ingredients_name ON ingredients(name);
